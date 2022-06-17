9d4356f0ed2022894afedd4012b0e7cfe51e81a3
Paco196300 <107424493+Paco196300@users.noreply.github.com>
2022-06-17 11:08:28 +0200

    Botoncerrar
    Se crea activa el boton de cerrar

--- Prueba/Login/Login.vb
+++ Prueba/Login/Login.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Login
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Se cerrará la ventana ...")
+        Close()
+    End Sub
 End Class
